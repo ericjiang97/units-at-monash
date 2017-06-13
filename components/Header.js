409f17906d61f0fcd257fc9fa732325a1f5ef819
Eric Jiang <eric.jiang@monash.edu>
2017-06-14 08:19:18 +1000

    Header and app.js

--- components/Header.js
+++ components/Header.js
@@ -3,28 +3,12 @@ import Expo from 'expo';
 import { StyleSheet, View } from 'react-native';
 import { Container, Content, ListItem, Text, CheckBox, Header } from 'native-base';
 
-export default class App extends React.Component {
+export default class Header extends React.Component {
   constructor(){
     super();
-    this.state = {
-      isReady: false,
-    };
-  }
-
-  async componentWillMount() {
-      await Expo.Font.loadAsync({
-        'Roboto': require('native-base/Fonts/Roboto.ttf'),
-        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      });
-      this.setState({isReady: true});
   }
 
   render() {
-    
-    if (!this.state.isReady) {
-      return <Expo.AppLoading />;
-    }
-
     return (
       <View>
         <Container>
@@ -39,9 +23,6 @@ export default class App extends React.Component {
                 </Right>
             </Header>
         </Container>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
