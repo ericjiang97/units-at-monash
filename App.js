c54844b3c365443031778e0c419f7f6171d3ff7d
Eric Jiang <eric.jiang@monash.edu>
2017-06-13 21:38:26 +1000

    initial updatee

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import Expo from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      isLoading: false,
+      store: configureStore(() => this.setState({ isLoading: false })),
+      isReady: false,
+    };
+  }
+
+  async componentWillMount() {
+      await Expo.Font.loadAsync({
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      });
+      this.setState({isReady: true});
+  }
+
   render() {
+    
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+    
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
